886df2d1fff066c21edd8c357563e23bd4ebea57
Jon Flores <jflore61@ucsc.edu>
2023-01-28 20:39:20 -0800

    new ccfavicon.ico added to public folder and pages/_document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import { CssBaseline } from "@nextui-org/react";
- 
+
 class MyDocument extends Document {
-  static async getInitialProps(ctx : DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
       styles: React.Children.toArray([initialProps.styles]),
     };
   }
- 
+
   render() {
     return (
       <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          <link rel="shortcut icon" href="/ccfavicon.ico" />
+          {/* {CssBaseline.flush()} */}
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -23,5 +32,5 @@ class MyDocument extends Document {
     );
   }
 }
- 
+
 export default MyDocument;
